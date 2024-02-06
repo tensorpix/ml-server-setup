199a260cc2c7c243ac3d642645165d137adb2b1d
Bartol Freskura <bartol@tensorpix.ai>
2024-02-06 12:57:35 +0100

    Support nvidia container toolkit

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo timedatectl set-timezone Europe/Zagreb
+TIMEZONE="Europe/Zagreb"
+NVIDIA_VERSION="535"
+
+sudo timedatectl set-timezone $TIMEZONE
 
 sudo apt update && sudo apt upgrade -y
 
@@ -16,13 +19,11 @@ sudo pip install -r requirements.txt
 
 # NVIDIA drivers
 # https://ubuntu.com/server/docs/nvidia-drivers-installation
-sudo ubuntu-drivers install nvidia:535-server
-sudo apt install nvidia-utils-535-server
-
+sudo ubuntu-drivers install nvidia:$NVIDIA_VERSION-server
+sudo apt install -y nvidia-utils-$NVIDIA_VERSION-server
 
 # Docker + docker compose
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get -y install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -33,9 +34,21 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
 sudo usermod -aG docker $USER
-
+echo "User $USER added to the 'docker' group."
 echo "Remember to add other users to the 'docker' group!"
+
+# NVIDIA container toolkit
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
